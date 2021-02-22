ca208078bc7b598c7757858327896a4d3fdea9e2
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-02-22 23:39:20 +0530

    Add files via upload

--- test_func.c
+++ test_func.c
@@ -2,6 +2,8 @@
 #include "test_func.h"
 #endif
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 /****************************************************************************************
 *Func desc : Test function which asserts conversion of pair number into corresponding colour pair 
 *Param     : pairnumber    - The pair number corresponding to the  expected major and minor color passed
