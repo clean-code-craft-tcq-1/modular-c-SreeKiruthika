1d1599b7f4331bd00b6ec3627bfa103943dec768
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-02-23 00:07:04 +0530

    Update test_func.c

--- test_func.c
+++ test_func.c
@@ -2,7 +2,7 @@
 #include "test_func.h"
 #endif
 
-extern const int MAX_COLORPAIR_NAME_CHARS = 16;
+MAX_COLORPAIR_NAME_CHARS = 16;
 
 /****************************************************************************************
 *Func desc : Test function which asserts conversion of pair number into corresponding colour pair 
