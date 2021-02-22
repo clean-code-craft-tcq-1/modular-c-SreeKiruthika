3866c4da66c56e1915172f9d77f7fbe84a70ac73
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-02-22 23:08:15 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 #include "test_func.h"
 #endif 
 
-#ifndef _COLOR_CODE_H
-#include "color_code.h"
-#endif
-
 #ifndef _STDIO_H
 #include <stdio.h>
 #endif
@@ -20,13 +16,17 @@ int main()
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
+	
 	/*Function to print manual for code coding*/
+	struct COLORPAIR manualprint;
+	
 	for(int pairnumber = 1; pairnumber <= 25; pairnumber++)
 	{
-		char *dummy ;
+		char *dummy;
 		printf("%d - ", pairnumber); 
-		ColorPairToString (GetColorFromPairNumber(pairnumber), dummy);
-		printf ("%s", pairnumber);
+                manualprint = GetColorFromPairNumber(pairnumber) 
+		ColorPairToString (manualprint , dummy);
+		printf ("%s", dummy);
 	}
     return 0;
 }
